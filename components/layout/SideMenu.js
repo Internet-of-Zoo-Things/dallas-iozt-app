22930c649b994801c7dc5b954633d872f1d27a3e
Andrew Schwenn <aschwenn@verizon.net>
2020-08-25 16:06:31 -0500

    minimizable side menu

--- components/layout/SideMenu.js
+++ components/layout/SideMenu.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import tw from 'twin.macro'
 import PropTypes from 'prop-types'
-import { Typography, Button, StyledLink } from '../primitives'
 import { withRouter } from 'next/router'
+import { Typography, Button, StyledLink } from '../primitives'
+
+const routes = [
+  {
+    href: '/',
+    title: 'Dashboard',
+    icon: 'dashboard'
+  },
+  {
+    href: '/users',
+    title: 'Users',
+    icon: 'user'
+  },
+  {
+    href: '/log',
+    title: 'Log',
+    icon: 'document'
+  }
+]
 
 const IconButton = styled(Button)`
   padding-left: 1.25rem;
@@ -19,6 +37,10 @@ const Logo = styled.img`
   margin-top: -1rem;
 `
 
+const LinkContainer = styled.div`
+  padding-left: ${({ active }) => (!active ? '4px' : '0')};
+`
+
 const MenuLink = ({
   href,
   icon,
@@ -29,14 +51,18 @@ const MenuLink = ({
 
   return (
     <StyledLink href={href}>
-      <div className={active ? 'border-l-4 border-primary' : ''}>
-        <div className="w-full h-16 flex items-center px-4 border-b border-background-darker hover:bg-background-darker">
+      <LinkContainer className={`${active ? 'border-l-4 border-primary' : ''}  hover:bg-background-darker`} active={active}>
+        <div className="w-full h-16 flex items-center px-4 border-b border-background-darker">
           <div className="pr-4">
             <Typography variant="icon" icon={icon} />
           </div>
-          <Typography variant="body" weight={active ? 'bold' : ''}>{title}</Typography>
+          {
+            title
+              ? <Typography variant="body" weight={active ? 'bold' : ''}>{title}</Typography>
+              : null
+          }
         </div>
-      </div>
+      </LinkContainer>
     </StyledLink>
   )
 }
@@ -48,27 +74,35 @@ MenuLink.propTypes = {
   router: PropTypes.object
 }
 
-const _ = ({ router }) => (
-  <div className="flex flex-col">
-    {/* header */}
-    <div className="flex items-center bg-primary text-white h-12 pl-4 w-64 border-b border-r border-primary-darker">
-      {/* left-aligned */}
-      <div className="flex flex-grow">
-        <Logo src={`${process.env.URL}/assets/images/logo.png`} alt="logo" />
+const _ = ({ router }) => {
+  const [open, setOpen] = useState(true)
+
+  return (
+    <div className={`flex flex-col ${open ? 'w-64' : 'w-16'}`}>
+      {/* header */}
+      <div className={`flex items-center bg-primary text-white h-12 ${open ? 'w-64 pl-4' : 'w-16'} border-b border-r border-primary-darker`}>
+        {
+          open
+            ? <div className="flex flex-grow">
+              <Logo src={`${process.env.URL}/assets/images/logo.png`} alt="logo" />
+            </div>
+            : null
+        }
+        <div className={`flex flex-row h-full ${!open ? 'w-full' : ''}`}>
+          <IconButton icon="menu" onClick={() => setOpen((prev) => (!prev))} className={open ? '' : 'w-full'} />
+        </div>
       </div>
-      {/* right-aligned */}
-      <div className="flex flex-row h-full">
-        <IconButton icon="menu" />
+      {/* menu links */}
+      <div className="flex flex-col flex-grow border-r border-background-darker">
+        {
+          routes.map((r, i) => (
+            <MenuLink href={r.href} title={open ? r.title : null} icon={r.icon} router={router} key={i} />
+          ))
+        }
       </div>
     </div>
-    {/* menu links */}
-    <div className="flex flex-col flex-grow border-r border-background-darker">
-      <MenuLink href="/" title="Dashboard" icon="dashboard" router={router} />
-      <MenuLink href="/users" title="Users" icon="user" router={router} />
-      <MenuLink href="/log" title="Log" icon="document" router={router} />
-    </div>
-  </div>
-)
+  )
+}
 _.propTypes = {
   /** router object containing current path */
   router: PropTypes.object
