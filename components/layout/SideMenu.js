ea0b26f721faaab931701bd5203491544d957a6e
Andrew Schwenn <aschwenn@verizon.net>
2020-08-25 14:34:26 -0500

    side menu accents

--- components/layout/SideMenu.js
+++ components/layout/SideMenu.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 import tw from 'twin.macro'
 import PropTypes from 'prop-types'
 import { Typography, Button, StyledLink } from '../primitives'
+import { withRouter } from 'next/router'
 
 const IconButton = styled(Button)`
   padding-left: 1.25rem;
@@ -22,23 +23,32 @@ const MenuLink = ({
   href,
   icon,
   title,
-  active
-}) => (
-  <StyledLink href={href}>
-    <div className="w-full h-12 flex items-center px-4 border-b border-background-darker hover:bg-background-darker">
-      <Typography variant="body">{title}</Typography>
-    </div>
-  </StyledLink>
-)
+  router
+}) => {
+  const active = router.pathname === href
+
+  return (
+    <StyledLink href={href}>
+      <div className={active ? 'border-l-4 border-primary' : ''}>
+        <div className="w-full h-16 flex items-center px-4 border-b border-background-darker hover:bg-background-darker">
+          <div className="pr-4">
+            <Typography variant="icon" icon={icon} />
+          </div>
+          <Typography variant="body" weight={active ? 'bold' : ''}>{title}</Typography>
+        </div>
+      </div>
+    </StyledLink>
+  )
+}
 MenuLink.propTypes = {
   href: PropTypes.string,
   icon: PropTypes.string,
   title: PropTypes.string,
-  /** is the active page */
-  active: PropTypes.bool
+  /** router object containing current path */
+  router: PropTypes.object
 }
 
-const _ = () => (
+const _ = ({ router }) => (
   <div className="flex flex-col">
     {/* header */}
     <div className="flex items-center bg-primary text-white h-12 pl-4 w-64 border-b border-r border-primary-darker">
@@ -53,11 +63,15 @@ const _ = () => (
     </div>
     {/* menu links */}
     <div className="flex flex-col flex-grow border-r border-background-darker">
-      <MenuLink href="/" title="Dashboard" />
-      <MenuLink href="/users" title="Users" />
-      <MenuLink href="/log" title="Log" />
+      <MenuLink href="/" title="Dashboard" icon="dashboard" router={router} />
+      <MenuLink href="/users" title="Users" icon="user" router={router} />
+      <MenuLink href="/log" title="Log" icon="document" router={router} />
     </div>
   </div>
 )
+_.propTypes = {
+  /** router object containing current path */
+  router: PropTypes.object
+}
 
-export default _
+export default withRouter(_)
