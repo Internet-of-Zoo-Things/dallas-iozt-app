5df6d69449c65cf7a8e55adbb49d55a4edfcb9b4
Andrew Schwenn <aschwenn@verizon.net>
2020-08-25 12:44:47 -0500

    set header icons

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,5 +1,17 @@
 import React from 'react'
-import { Typography } from '../primitives'
+import styled from 'styled-components'
+import tw from 'twin.macro'
+import { Typography, Button } from '../primitives'
+
+const IconButton = styled(Button)`
+  padding-left: 1.25rem;
+  padding-right: 1.25rem;
+  height: 100%;
+
+  &&& {
+    ${tw`hover:bg-primary-darker hover:opacity-100`}
+  }
+`
 
 const _ = () => (
   <div className="flex justify-start items-center bg-primary text-white h-12 px-4">
@@ -8,8 +20,10 @@ const _ = () => (
       <Typography variant="h6">Dashboard</Typography>
     </div>
     {/* right-aligned */}
-    <div className="flex flex-row">
-      icons here
+    <div className="flex flex-row h-full">
+      <IconButton icon="search" />
+      <IconButton icon="notifications" />
+      <IconButton icon="user" />
     </div>
   </div>
 )
