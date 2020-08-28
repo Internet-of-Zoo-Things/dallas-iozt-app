87d50e1cfad5bc584ddd930a5e748019ee8c52b8
Andrew Schwenn <aschwenn@verizon.net>
2020-08-28 11:23:04 -0500

    added temporary favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Head from 'next/head'
 import { ThemeProvider } from '../components/providers'
 import '../public/index.css'
 
 const MyApp = ({ Component, pageProps }) => (
   <ThemeProvider>
+    <Head>
+      <link rel="shortcut icon" href="/favicon.ico" />
+    </Head>
     <Component {...pageProps} />
   </ThemeProvider>
 )
