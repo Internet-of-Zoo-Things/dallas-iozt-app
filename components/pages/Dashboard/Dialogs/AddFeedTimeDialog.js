4890285a0ab31a1c90c195d81fb59d74d61cbbd9
psittacine <psittacine714@gmail.com>
2020-09-21 15:41:08 -0500

    Add dialog form to schedule individual feed time

---
+++ components/pages/Dashboard/Dialogs/AddFeedTimeDialog.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Dialog } from '@blueprintjs/core'
+import { Form } from '../../../primitives'
+import { InputTypes } from '../../../../utils/models'
+
+const _ = ({ isOpen, close }) => {
+  // TODO:  Add api interaction
+
+  return (
+    <Dialog
+      icon="plus"
+      onClose={() => close()}
+      title="Schedule Feed Time"
+      isOpen={isOpen}
+    >
+      <div className="w-full p-6">
+        <Form
+          onSubmit={(data) => {
+            // TODO:  Implement form submit to db
+            // console.log(JSON.stringify(data))
+          }}
+          submitLoading={false}
+          fields={[
+            {
+              label: 'Feeder',
+              id: 'feeder',
+              required: true,
+              type: InputTypes.SELECT,
+              placeholder: 'Select Feeder',
+              // TODO:  Use dynamic list of possible feeders from db
+              items: [
+                { label: 'Feeder A' },
+                { label: 'Feeder B' },
+                { label: 'Feeder C' }
+              ]
+            },
+            {
+              label: 'Food Quantity (lbs)',
+              id: 'quantity',
+              required: true,
+              type: InputTypes.NUMERIC,
+              placeholder: 'Enter amount of food to dispense in lbs',
+              validator: (val) => /^-?\d+\.?\d*$/.test(val) && val > 0
+            },
+            {
+              // TODO:  Create InputTypes for timestamp and edit this accordingly
+              label: 'Time',
+              id: 'timestamp',
+              required: true,
+              type: InputTypes.TEXT,
+              placeholder: 'Enter time to dispense feeding'
+            }
+          ]}
+        />
+      </div>
+    </Dialog>
+  )
+}
+_.propTypes = {
+  isOpen: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+  data: PropTypes.object.isRequired
+}
+
+export default _
