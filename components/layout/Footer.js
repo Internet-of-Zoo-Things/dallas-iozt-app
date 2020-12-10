bf40447b1d4f05778f3db8f6ba65f4b9a8ad7c64
Andrew Schwenn <aschwenn@verizon.net>
2020-12-09 21:22:32 -0600

    versioning in footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -7,42 +7,47 @@ import {
 } from '../primitives'
 
 const _ = () => (
-  <div className="flex flex-row justify-end items-center bg-background-darker text-gray p-4 relative z-10">
-    {
-      process.env.NODE_ENV !== 'production'
-        ? <>
-          <div className="mr-2">
-            <StyledLink href="/graphql">
-              <Button minimal className="h-full" icon="graph">GraphQL Playground</Button>
+  <div className="flex items-center bg-background-darker text-gray p-4 relative z-10">
+    <div className="flex items-center pr-4">
+      <Typography variant="subtitle">IoZT version 0.1.0</Typography>
+    </div>
+    <div className="flex flex-grow justify-end items-center pl-4">
+      {
+        process.env.NODE_ENV !== 'production'
+          ? <>
+            <div className="mr-2">
+              <StyledLink href="/graphql">
+                <Button minimal className="h-full" icon="graph">GraphQL Playground</Button>
+              </StyledLink>
+            </div>
+            <div className="mr-2">
+              <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app" external>
+                <Button minimal className="h-full" icon="code">View the source</Button>
+              </StyledLink>
+            </div>
+          </>
+          : null
+      }
+      <Popover content={
+        <div className="p-3 w-48 break-normal text-center">
+          <Typography variant="body" className="mb-2">
+            Please create a new issue at
+            <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app/issues" external className="mx-1 text-primary-darker">
+              this link
             </StyledLink>
-          </div>
-          <div className="mr-2">
-            <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app" external>
-              <Button minimal className="h-full" icon="code">View the source</Button>
+            and label it as a <strong>Bug</strong>!
+          </Typography>
+          <Typography variant="subtitle" className="opacity-75">
+            <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app/blob/develop/Report-a-Bug.md" external className="mx-1 text-primary-darker">
+              Click here
             </StyledLink>
-          </div>
-        </>
-        : null
-    }
-    <Popover content={
-      <div className="p-3 w-48 break-normal text-center">
-        <Typography variant="body" className="mb-2">
-          Please create a new issue at
-          <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app/issues" external className="mx-1 text-primary-darker">
-            this link
-          </StyledLink>
-          and label it as a <strong>Bug</strong>!
-        </Typography>
-        <Typography variant="subtitle" className="opacity-75">
-          <StyledLink href="https://github.com/Internet-of-Zoo-Things/dallas-iozt-app/blob/develop/Report-a-Bug.md" external className="mx-1 text-primary-darker">
-            Click here
-          </StyledLink>
-          for help creating an issue ticket.
-        </Typography>
-      </div>
-    }>
-      <Button minimal className="h-full" icon="warning-sign">Report a bug</Button>
-    </Popover>
+            for help creating an issue ticket.
+          </Typography>
+        </div>
+      }>
+        <Button minimal className="h-full" icon="warning-sign">Report a bug</Button>
+      </Popover>
+    </div>
   </div>
 )
 
