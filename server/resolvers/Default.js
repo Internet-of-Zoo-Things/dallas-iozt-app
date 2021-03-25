f0268cdb7f6fe0065ad82d210a5d4240670dd816
Andrew Schwenn <aschwenn@verizon.net>
2021-03-24 20:18:46 -0500

    updated defaults logic

--- server/resolvers/Default.js
+++ server/resolvers/Default.js
@@ -1,25 +1,65 @@
 const { ApolloError } = require('apollo-server-express')
 const { GraphQLScalarType } = require('graphql')
 const { Kind } = require('graphql/language')
+const { writeLog } = require('../../utils/functions/api')
 
 const Default = {
   Query: {
     async defaults(parent, filter, { models }) {
-      return models.Default.find(filter)
+      return new Promise((resolve, reject) => {
+        models.Default.find(filter, (err, defaults) => {
+          if (err) reject(err)
+          else resolve(defaults)
+        })
+      })
         .catch((err) => { throw new ApolloError(err) })
     }
   },
   Mutation: {
     async createDefault(parent, args, { models }) {
-      return models.Default.create(args)
+      return new Promise((resolve, reject) => {
+        models.Default.insert(args, (err, _default) => {
+          if (err) reject(err)
+          else {
+            writeLog(models, `Created default "${args.name}"`, 'default')
+              .then(() => { resolve(_default) })
+          }
+        })
+      })
         .catch((err) => { throw new ApolloError(err) })
     },
     async updateDefault(parent, { _id, ...args }, { models }) {
-      return models.Default.findByIdAndUpdate(_id, args, { new: true })
+      return new Promise((resolve, reject) => {
+        models.Default.update({ _id }, { $set: args }, { multi: false }, (err1) => {
+          if (err1) reject(err1)
+          else {
+            models.Default.findOne({ _id }, (err2, _default) => {
+              if (err2) reject(err2)
+              else {
+                writeLog(models, `Updated default "${args.name}"`, 'default')
+                  .then(() => { resolve(_default) })
+              }
+            })
+          }
+        })
+      })
         .catch((err) => { throw new ApolloError(err) })
     },
     async deleteDefault(parent, { _id }, { models }) {
-      return models.Default.findByIdAndDelete(_id)
+      return new Promise((resolve, reject) => {
+        models.Default.findOne({ _id }, (err1, _default) => {
+          if (err1) reject(err1)
+          else {
+            models.Default.remove({ _id }, { multi: false }, (err2) => {
+              if (err2) reject(err2)
+              else {
+                writeLog(models, `Deleted default "${_default.name}"`, 'default')
+                  .then(() => { resolve(_default) })
+              }
+            })
+          }
+        })
+      })
         .catch((err) => { throw new ApolloError(err) })
     }
   },
