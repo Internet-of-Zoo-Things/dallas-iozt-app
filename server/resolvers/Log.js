822f9ec944a496d9a9c1f17c7ac8b9b7fdfcbc73
Andrew Schwenn <aschwenn@verizon.net>
2021-03-24 21:53:57 -0500

    updated logs logic

--- server/resolvers/Log.js
+++ server/resolvers/Log.js
@@ -3,14 +3,39 @@ const { ApolloError } = require('apollo-server-express')
 const Log = {
   Query: {
     async logs(parent, { tag, limit, skip }, { models }) {
-      return models.Log.find(tag ? { tag } : {}).sort({ timestamp: -1 }).limit(limit).skip(skip || 0)
+      return new Promise((resolve, reject) => {
+        models.Log.find(tag ? { tag } : {}).sort({ timestamp: -1 }).limit(limit).skip(skip || 0)
+          .exec((err, logs) => {
+            if (err) reject(err)
+            else resolve(logs)
+          })
+      })
         .catch((err) => { throw new ApolloError(err) })
     },
     async logCount(parent, { tag }, { models }) {
-      return models.Log.countDocuments(tag ? { tag } : {}).catch((err) => { throw new ApolloError(err) })
+      return new Promise((resolve, reject) => {
+        models.Log.count(tag ? { tag } : {}, (err, count) => {
+          if (err) reject(err)
+          else resolve(count)
+        })
+      })
+        .catch((err) => { throw new ApolloError(err) })
     },
     async logTags(parent, args, { models }) {
-      return models.Log.distinct('tag').catch((err) => { throw new ApolloError(err) })
+      return new Promise((resolve, reject) => {
+        models.Log.find({}, (err, logs) => {
+          if (err) reject(err)
+          else {
+            /** use dict for o(1) lookup and insertion */
+            const tagsDict = logs.reduce((dict, curr) => {
+              if (!(curr.tag in dict)) dict[curr.tag] = true
+              return dict
+            }, {})
+            resolve(Object.keys(tagsDict))
+          }
+        })
+      })
+        .catch((err) => { throw new ApolloError(err) })
     }
   }
 }
